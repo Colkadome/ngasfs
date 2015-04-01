fba70013f04b89bbf5022107a39610c019eba28b
Colkadome <josephdunne1993@gmail.com>
2015-04-01 17:24:41 +0800

    recursive filepath building:

--- push.py
+++ push.py
@@ -10,6 +10,12 @@ import requests
 database = 'dumpfs.sqlite'
 SERVER_LOCATION = 'http://ec2-54-152-35-198.compute-1.amazonaws.com:7777/'
 
+def getListIndex(list,col,val):
+	for i in range(len(list)):
+		if list[i][col] == val:
+			return i
+	return 0
+
 def main():
 
 	# check if the number of arguments is correct
@@ -31,24 +37,51 @@ def main():
 		print "Error: Database doesn't exist!"
 		exit()
 
+	# connect to server and get file names
+	T = atpy.Table(SERVER_LOCATION + 'QUERY?query=files_list&format=list',type='ascii')
+	fileNames = []
+	for s in T['col3'][3:]:		# Get every entry from the 4th and onwards (to skip NGAS's first three lines)
+		fileNames.append(s)
+
     # Execute code with connection
+	uploadCount = 0
 	with con:
 		cur = con.cursor() # create cursor
-		# for each filename in dentry (that is NOT a directory), upload to ARCHIVE
-		for entry in cur.execute("SELECT filename FROM dentry WHERE inode_num IN (SELECT DISTINCT parent_id FROM data_list)"):
-			# get arguments to send to ARCHIVE.
-			# mime_type = "application/octet-stream" to upload any file.
-			filename = entry[0]
-			mime_type = "application/octet-stream"
-			# construct header.
-			headers = {"Content-type":mime_type,"Content-Disposition":'filename="'+filename+'"'}
-			# print something.
-			print "--------------------"
-			print "Uploading: " + filename
-			print "--------------------"
-			# send POST request and print response.
-			response = requests.post(SERVER_LOCATION + "ARCHIVE", headers=headers, files={filename: open(mountDir+"/"+filename, mode='rb')})
-			print response.text
+		# execute sql query.
+		# the query joins inode and dentry BY inode_num. parent_id references inode.id.
+		cur.execute("SELECT inode.id, filename, parent_id FROM dentry, inode WHERE dentry.inode_num = inode.inode_num GROUP BY filename ORDER BY inode.id")
+		result = cur.fetchall() # save sql query's result
+		for entry in result:
+			# recursively search through entry's parents to gather full file path
+			fpath = entry[1]
+			curr = entry	# current entry
+			while 1:
+				pId = curr[2] # get parent ID
+				if pId==1:	# if the parent is mount directory (id = 1), exit loop
+					break
+				curr = result[getListIndex(result,1,pId)]	# find parent entry, and move to it
+				fpath = curr[1] + "/" + fpath	# append parent's name to fpath
+			fpath = mountDir + "/" + fpath	# append mount to path
+
+			# check if file does not exist on the server, and is not a directory!
+			fname = entry[1]
+			if fname not in fileNames and os.path.isfile(fpath):
+				uploadCount += 1
+				# mime_type = "application/octet-stream" to upload any file.
+				mime_type = "application/octet-stream"
+				# construct header.
+				headers = {"Content-type":mime_type,"Content-Disposition":'filename="'+fname+'"'}
+				# print stuff for user.
+				#print "--------------------"
+				print "Uploading: " + fpath
+				#print "--------------------"
+				# send POST request and print response.
+				response = requests.post(SERVER_LOCATION + "ARCHIVE", headers=headers, files={fname: open(fpath, mode='rb')})
+				# print response.text
+	if uploadCount > 0:
+		print str(uploadCount) + " file(s) successfully uploaded to NGAS."
+	else:
+		print "All filenames exist on server."
 
 if __name__ == "__main__":
     main()
