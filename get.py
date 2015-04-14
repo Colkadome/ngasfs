3217d71029db5c5d13884aa4e64a10f9cc816649
Colkadome <josephdunne1993@gmail.com>
2015-04-14 15:42:04 +0800

    better getting

--- get.py
+++ get.py
@@ -11,7 +11,7 @@ import ntpath
 from urllib2 import urlopen, URLError, HTTPError
 
 # add file(s) from NGAS to the FS
-def getFile(sLoc, fs_id, patterns):
+def getFiles(sLoc, fs_id, patterns):
 
     # check if database exists
     con = None
@@ -35,82 +35,67 @@ def getFile(sLoc, fs_id, patterns):
     if not isinstance(patterns, list):
         patterns = [patterns]
 
+    # execute some code using con (CODE FROM FILLDB)
+    cur.execute("SELECT * FROM dentry ORDER BY parent_id DESC")
+    parent_id = cur.fetchone() #Get highest existng parent_id
+    if parent_id == None:
+        parent_id = 1 #If None then set to 1
+    else:
+        parent_id = parent_id[1]
+    cur.execute("SELECT * FROM inode ORDER BY id DESC")
+    inodeID = cur.fetchone()[0] #Get highest inode id
+    cur.execute("SELECT * FROM inode ORDER BY inode_num DESC")
+    inode_num = cur.fetchone()[1] #Get highest inode_num
+    cur.execute("SELECT * FROM dentry ORDER BY id DESC")
+    dentryID = cur.fetchone() #Get highest dentry id
+    if dentryID == None:
+        dentryID = 0 #If None then set to 0
+    else:
+        dentryID = dentryID[0]
+    cur.execute("SELECT * FROM data_list ORDER BY id DESC")
+    data_listID = cur.fetchone() #Get highests data_list id
+    if data_listID == None:
+        data_listID = 0 #If None then set to 0
+    else:
+        data_listID = data_listID[0]
+
     # add filenames from server (and remove duplicate names)
-    T = numpy.array([], dtype=[('col1', 'S32'), ('col2', 'S37'), ('col3', 'S18'),
-        ('col4', 'S19'), ('col5', 'S24'), ('col6', 'S20'),
-        ('col7', 'S28'), ('col8', 'S22'), ('col9', 'S23'),
-        ('col10', 'S17'), ('col11', 'S19'), ('col12', 'S22'),
-        ('col13', 'S22'), ('col14', 'S24'), ('col15', 'S18')])
+    uploadCount = 0
     for p in patterns:
         # gather new files using pattern
         print "-- Matching " + p
-        temp = atpy.Table(sLoc + 'QUERY?query=files_like&format=list&like='+p,type='ascii')[3:]   # get everything past result 3
+        T = atpy.Table(sLoc + 'QUERY?query=files_like&format=list&like='+p,type='ascii')[3:]   # get everything past result 3
 
         # iterate through names.
         # add entries that are not in DB, or have not been added previously.
-        names = temp['col3']
+        names = T['col3']
         for i in range(len(names)):
             if names[i] not in ignore:
                 print "Adding " + names[i]
-                T = numpy.append(T, temp[i])
+                uploadCount += 1
+                
+                cTime = T[i]['col14'].replace("T", " ")
+                cTime = time.mktime(datetime.datetime.strptime(cTime, "%Y-%m-%d %H:%M:%S.%f").timetuple())
+                iTime = T[i]['col9'].replace("T", " ")
+                iTime = time.mktime(datetime.datetime.strptime(iTime, "%Y-%m-%d %H:%M:%S.%f").timetuple())
+                iString = "INSERT INTO inode VALUES({0}, {1}, 1, 1000, 1000, {2}, {2}, {3}, {4}, 33060, 0)".format(inodeID+i+1, inode_num+i+1, iTime, cTime, T[i]['col6'])
+                deString = "INSERT INTO dentry VALUES({0}, {1}, '{2}', {3}, '{4}')".format(dentryID+i+1, parent_id, T[i]['col3'], inode_num+i+1, sLoc)
+                cur.execute(iString)
+                cur.execute(deString)
             else:
                 print "Ignored " + names[i] + ", already in FS."
         # new names are now ignored
         ignore = ignore.union(set(names))
 
-    if len(T) > 0 :
-
-        # gather file properties
-        s_fileName = T['col3']
-        s_size = T['col6']
-        s_ctime = T['col14']
-        s_itime = T['col9']
-
-        # execute some code using con (CODE FROM FILLDB)
-        cur.execute("SELECT * FROM dentry ORDER BY parent_id DESC")
-        parent_id = cur.fetchone() #Get highest existng parent_id
-        if parent_id == None:
-            parent_id = 1 #If None then set to 1
-        else:
-            parent_id = parent_id[1]
-        cur.execute("SELECT * FROM inode ORDER BY id DESC")
-        inodeID = cur.fetchone()[0] #Get highest inode id
-        cur.execute("SELECT * FROM inode ORDER BY inode_num DESC")
-        inode_num = cur.fetchone()[1] #Get highest inode_num
-        cur.execute("SELECT * FROM dentry ORDER BY id DESC")
-        dentryID = cur.fetchone() #Get highest dentry id
-        if dentryID == None:
-            dentryID = 0 #If None then set to 0
-        else:
-            dentryID = dentryID[0]
-        cur.execute("SELECT * FROM data_list ORDER BY id DESC")
-        data_listID = cur.fetchone() #Get highests data_list id
-        if data_listID == None:
-            data_listID = 0 #If None then set to 0
-        else:
-            data_listID = data_listID[0]
-
-        # iterate through T and add files to FS
-        for i in range(len(T)):
-
-            # fill database with data from server (FILLDB CODE)
-            cTime = s_ctime[i].replace("T", " ")
-            cTime = time.mktime(datetime.datetime.strptime(cTime, "%Y-%m-%d %H:%M:%S.%f").timetuple())
-            iTime = s_itime[i].replace("T", " ")
-            iTime = time.mktime(datetime.datetime.strptime(iTime, "%Y-%m-%d %H:%M:%S.%f").timetuple())
-            iString = "INSERT INTO inode VALUES({0}, {1}, 1, 1000, 1000, {2}, {2}, {3}, {4}, 33060, 0)".format(inodeID+i+1, inode_num+i+1, iTime, cTime, s_size[i])
-            deString = "INSERT INTO dentry VALUES({0}, {1}, '{2}', {3}, {4})".format(dentryID+i+1, parent_id, s_fileName[i], inode_num+i+1, "'"+sLoc+"'")
-            cur.execute(iString)
-            cur.execute(deString)
-
-        # print stuff for user
-        print "-- " + str(len(T)) + " file(s) added to " + fs_id
-    else:
-        print "-- No files added to " + fs_id
-
-    # commit and close connection to DB
+    # commit database changes
     con.commit()
     con.close()
+
+    # print info for the user
+    if uploadCount > 0:
+        print "-- " + str(uploadCount) + " file(s) successfully added to " + fs_id
+    else:
+        print "-- All files exist on " + fs_id
 
 # Download a FS
 def getFS(sLoc, fs_id):
@@ -130,5 +115,5 @@ def getFS(sLoc, fs_id):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print "USAGE: post.py <server_loc> <fs_id>"
-    getFS(sys.argv[1],sys.argv[2])
+        print "USAGE: get.py <server_loc> <fs_id> <patterns>"
+    getFiles(sys.argv[1],sys.argv[2],sys.argv[3])
